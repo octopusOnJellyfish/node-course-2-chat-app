7f89863eb2eb0ab29c8e03a2ee9928990985dbc1
Rodrigo <rodrigonasif@gmail.com>
2018-08-31 15:54:05 -0300

    fixed port issue

--- server/server.js
+++ server/server.js
@@ -7,6 +7,6 @@ const port = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`Started on port 3000`);
 });
