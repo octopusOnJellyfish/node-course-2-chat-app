aaa768074a46794d05504ce1dd7bdc0737120f8f
Rodrigo <rodrigonasif@gmail.com>
2018-08-31 17:20:55 -0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    to:'jen@gmail.com',
-    text: 'hey. this is andrew.'
-  });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
-
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
@@ -15,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'rodrigo@example.com',
-    text: 'Hey whats goin',
-    createdAt: 1234
-  });
-
   socket.on('createMessage', (message) => {
     console.log('create new message', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
